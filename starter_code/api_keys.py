4d3550da409f20b02e5cafc261fb134b29f261f8
Taylor Marino <55257639+marinotem@users.noreply.github.com>
2022-12-07 19:21:18 -0500

    Updating with my code

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "2165120a4bf7c4f692519011971e894b"
+weather_api_key= "2165120a4bf7c4f692519011971e894b"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "AIzaSyBuebDVefnJp9P6m8wIJ8A6WPjQTQVusao"
